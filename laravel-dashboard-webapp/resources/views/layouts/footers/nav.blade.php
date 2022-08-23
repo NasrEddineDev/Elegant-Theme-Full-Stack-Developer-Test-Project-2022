69d41f88501afd16ffa8adeeb1c332c8324ac6b0
nasreddine <nasreddine.guelfout@gmail.com>
2022-08-23 05:45:02 +0100

    adding images

--- laravel-dashboard-webapp/resources/views/layouts/footers/nav.blade.php
+++ laravel-dashboard-webapp/resources/views/layouts/footers/nav.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="col-xl-6">
             <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
                 </li>
                 <li class="nav-item">
@@ -20,7 +20,7 @@
                 </li>
                 <li class="nav-item">
                     <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-                </li>
+                </li> -->
             </ul>
         </div>
     </div>
